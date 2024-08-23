90ea5d7c0cd10e98364aa37b9865ba2bb378cca8
vipin-chauhan-2004 <vipinchauhan2004@gmail.com>
2024-08-23 16:53:09 +0000

    learned about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=14453
+let accountEmail="vipin@gmail.com"
+var accountPassword="12345678"
+accountCity="Lucknow"
+
+accountEmail="asdf@gmail.com"
+accountPassword="87654321"
+accountCity="Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
