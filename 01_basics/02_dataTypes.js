fd334e134dbaa6b369f12e6e9c85b2f4fe051fc8
vipin-chauhan-2004 <vipinchauhan2004@gmail.com>
2024-08-23 17:15:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";  /* treat JS code as newer version also if written this then you cannot use "use no strict" and
+in modern browser we don't even need to write this */
+//alert(3+3)//using nodejs and not browser so popup wont come here
+console.log(3+3)
+console.log("Vipin")
+
+
+//symbol=> to differentiate between unique components
+ 
