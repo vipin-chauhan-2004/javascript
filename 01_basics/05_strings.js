5fb9841afd0136889c2536fc76110144cbc05987
vipin-chauhan-2004 <vipinchauhan2004@gmail.com>
2024-10-01 13:14:34 +0000

    learned strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+let name="Hello,World!"
+// console.log(name.substring(-5,0))
+let arr=[10,20,30,40,50]
+// console.log(arr.slice(6,10))
+// console.log(arr.slice(5,10))
+console.log(name.slice(-3,-5))
+// console.log(name.charAt(-3))   //not allowed
