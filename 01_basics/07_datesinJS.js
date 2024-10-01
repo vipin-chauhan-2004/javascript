c72522363fcb6df8437fcc05e85de96800c7e815
vipin-chauhan-2004 <vipinchauhan2004@gmail.com>
2024-10-01 17:30:08 +0000

    dateAndTime

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,18 @@
+let myDate= new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString('en-IN'))
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+// let myyDate=new Date()
+// console.log(Date.now())
+// let newDate=new Date()
+// console.log(newDate.getTimezoneOffset())
+let newDate = new Date();
+console.log(newDate.toString()); // Outputs the full date string including the time zone
+console.log(newDate.getTimezoneOffset()); // Outputs the timezone offset in minutes
+
